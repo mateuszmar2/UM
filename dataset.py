d4865d0d7292bd6ded783627b78257f592e0ca87
Mateusz Marciniec <mateuszmar2@gmail.com>
2023-12-09 22:42:16 +0100

    Add FVC dataset
    This class can read images in .tif format.
    It is intended for data from fingerprint verification competition.
    Dataset should be restructured, each finger should be a different
    class/directory, for example:
        ../datasets/FVC2002_DB1_B_split
        ├── 101
        │   ├── 101_1.tif
        │   ├── 101_2.tif
        │   ├── 101_3.tif
        │   ├── 101_4.tif
        │   ├── 101_5.tif
        │   ├── 101_6.tif
        │   ├── 101_7.tif
        │   └── 101_8.tif
        ├── 102
        │   ├── 102_1.tif
        │   ├── 102_2.tif
        │   ├── 102_3.tif
        │   ├── 102_4.tif
        │   ├── 102_5.tif
        │   ├── 102_6.tif
        │   ├── 102_7.tif
        │   └── 102_8.tif
        ├── 103
        │   ├── 103_1.tif
        │   ├── 103_2.tif
        │   ├── 103_3.tif
        │   ├── 103_4.tif
        │   ├── 103_5.tif
        │   ├── 103_6.tif
        │   ├── 103_7.tif
        │   └── 103_8.tif

---
+++ dataset.py
@@ -0,0 +1,52 @@
+import glob
+import torch
+from torch.utils.data import Dataset, DataLoader
+import matplotlib.pyplot as plt
+from torchvision.transforms import ToTensor
+import rasterio
+
+
+class FVCDataset(Dataset):
+    def __init__(self):
+        self.imgs_path = "../datasets/FVC2002_DB1_B_split/"
+        file_list = glob.glob(self.imgs_path + "*")
+        # print(file_list)
+        self.data = []
+        for class_path in file_list:
+            # in our case numbers are class names, so they can be used as class_id
+            class_id = int(class_path.split("/")[-1])
+            for img_path in glob.glob(class_path + "/*.tif"):
+                self.data.append([img_path, class_id])
+        # print(self.data)
+        # self.img_dim = (416, 416)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        img_path, class_id = self.data[idx]
+        img = rasterio.open(img_path)
+        img = img.read(1)
+        # plt.imshow(img, cmap="gray")
+        # plt.show()
+        img_tensor = ToTensor()(img)
+        class_id = torch.tensor([class_id])
+        return img_tensor, class_id
+        # return img_tensor.float(), class_id.float()
+
+
+if __name__ == "__main__":
+    dataset = FVCDataset()
+    data_loader = DataLoader(dataset, batch_size=4, shuffle=True)
+    for imgs, labels in data_loader:
+        print("Batch of images has shape: ", imgs.shape)
+        print("Batch of labels has shape: ", labels.shape)
+
+    images, labels = next(iter(data_loader))
+    image = images[0]
+    print(image.shape)
+    image = image.permute(1, 2, 0)
+    print(image.shape)
+    plt.imshow(image.numpy())
+    plt.gray()
+    plt.show()
