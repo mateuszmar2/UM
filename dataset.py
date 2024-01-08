4965a264b0d596ff8de3bdea6823b62a1341d418
Mateusz Marciniec <mateuszmar2@gmail.com>
2024-01-08 23:50:44 +0100

    dataset: Cleanup and improve plotting

--- dataset.py
+++ dataset.py
@@ -10,14 +10,12 @@ class FVCDataset(Dataset):
     def __init__(self):
         self.imgs_path = "../datasets/FVC2002_DB1_B_split/"
         file_list = glob.glob(self.imgs_path + "*")
-        # print(file_list)
         self.data = []
         for class_path in file_list:
             # in our case numbers are class names, so they can be used as class_id
             class_id = int(class_path.split("/")[-1])
             for img_path in glob.glob(class_path + "/*.tif"):
                 self.data.append([img_path, class_id])
-        # print(self.data)
         self.img_dim = (784, 784)
 
     def __len__(self):
@@ -27,28 +25,43 @@ class FVCDataset(Dataset):
         img_path, class_id = self.data[idx]
         img = Image.open(img_path)
         img = img.resize(self.img_dim, Image.LANCZOS)
-        # img = rasterio.open(img_path)
-        # img = img.read(1)
-        # plt.imshow(img, cmap="gray")
-        # plt.show()
         img_tensor = ToTensor()(img)
         class_id = torch.tensor([class_id])
         return img_tensor, class_id
-        # return img_tensor.float(), class_id.float()
 
 
 if __name__ == "__main__":
     dataset = FVCDataset()
-    data_loader = DataLoader(dataset, batch_size=4, shuffle=True)
-    for imgs, labels in data_loader:
-        print("Batch of images has shape: ", imgs.shape)
-        print("Batch of labels has shape: ", labels.shape)
+    data_loader = DataLoader(dataset, batch_size=4, shuffle=False)
 
-    images, labels = next(iter(data_loader))
-    image = images[0]
-    print(image.shape)
-    image = image.permute(1, 2, 0)
-    print(image.shape)
-    plt.imshow(image.numpy())
-    plt.gray()
+    # load images to dict
+    fingerprints = {}
+    for batch in data_loader:
+        for img, label in zip(batch[0], batch[1]):
+            img = img.permute(1, 2, 0)
+            label = label.item()
+            print(f"Image shape: {img.shape}, label: {label}")
+            fingerprints.setdefault(label, []).append(img)
+
+    width = max([len(fingerprints[key]) for key in fingerprints.keys()])
+    height = fingerprints.__len__()
+    # reduce size to make it more readable
+    if height > 4:
+        width = 4
+        height = 4
+
+    print(f"Width: {width}, height: {height}")
+
+    finger_numbers = list(fingerprints.keys())
+    fig = plt.figure(figsize=(width, height))
+
+    for i in range(height):
+        for j in range(width):
+            fig.add_subplot(height, width, i * width + j + 1)
+            plt.gray()
+            plt.imshow(fingerprints[finger_numbers[i]][j])
+            plt.axis("off")
+            plt.title(f"Class: {finger_numbers[i]} - {j}")
+
+    fig.tight_layout()
     plt.show()
