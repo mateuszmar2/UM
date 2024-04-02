668565f637e131466614c5c9e384f6e06af1af52
Mateusz Marciniec <mateuszmar2@gmail.com>
2024-04-03 00:48:33 +0200

    dataset.py: Introduce image preprocessing

--- dataset.py
+++ dataset.py
@@ -5,14 +5,16 @@ from torch.utils.data import Dataset, DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
 import matplotlib.pyplot as plt
 from torchvision.transforms import ToTensor
-from PIL import Image
+from PIL import Image, ImageChops
+import cv2
+import numpy as np
 
 
 class FVCDataset(Dataset):
     def __init__(
         self,
         root="FVC2002_dataset/",
-        img_dim=(784, 784),
+        img_dim=(300, 300),
         single_class=False,
         fingerprint_database="1",
         pca_transform=False,
@@ -20,8 +22,8 @@ class FVCDataset(Dataset):
         self.imgs_path = root + fingerprint_database
         self.img_dim = img_dim
         file_list = glob.glob(self.imgs_path + "*")
+        file_list = sorted(file_list)
         self.data = []
-
         self.pca_transform = pca_transform
 
         if pca_transform:
@@ -44,18 +46,72 @@ class FVCDataset(Dataset):
 
     def __getitem__(self, idx):
         img_path, class_id = self.data[idx]
-        img = Image.open(img_path)
-        img = img.resize(self.img_dim, Image.LANCZOS)
-        if self.pca_transform:
-            img = pca_transform(img, self.pca_components)
-
+        img = preprocess_image(img_path, self.img_dim, self.pca_transform)
         img_tensor = ToTensor()(img)
         class_id = torch.tensor([class_id])
         return img_tensor, class_id
 
 
+def preprocess_image(img_path, img_dim, pca_transform):
+    img = Image.open(img_path)
+    # TODO remove this
+    # img_resize = img.resize(img_dim, Image.LANCZOS)
+    # return img_resize
+    img_dilatation = image_dilataion(img)
+    img_thresh = adaptive_gaussian_thresholding(img_dilatation)
+    img_crop = crop_whitespace(img_thresh)
+    img_resize = img_crop.resize(img_dim, Image.LANCZOS)
+
+    if pca_transform:
+        img_resize = pca_transform(img_resize, self.pca_components)
+
+    return img_resize
+
+
+def image_dilataion(img, dilatation_size=1):
+    dilation_shape = cv2.MORPH_ELLIPSE
+    element = cv2.getStructuringElement(
+        dilation_shape,
+        (2 * dilatation_size + 1, 2 * dilatation_size + 1),
+        (dilatation_size, dilatation_size),
+    )
+    return Image.fromarray(cv2.dilate(np.array(img), element))
+
+
+def adaptive_gaussian_thresholding(img):
+    img = np.array(img)
+
+    thresh = cv2.adaptiveThreshold(
+        img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 199, 5
+    )
+
+    return Image.fromarray(thresh)
+
+
+def image_erosion(img, erosion_size=1):
+    erosion_shape = cv2.MORPH_CROSS
+    element = cv2.getStructuringElement(
+        erosion_shape,
+        (2 * erosion_size + 1, 2 * erosion_size + 1),
+        (erosion_size, erosion_size),
+    )
+    return Image.fromarray(cv2.erode(np.array(img), element))
+
+
+def crop_whitespace(img):
+    bg = Image.new(img.mode, img.size, img.getpixel((0, 0)))
+    diff = ImageChops.difference(img, bg)
+    diff = ImageChops.add(diff, diff, 2.0, -100)
+    diff = image_erosion(diff, 1)
+    bbox = diff.getbbox(alpha_only=False)
+    if bbox:
+        img = img.crop(bbox)
+    else:
+        print("Empty image")
+    return img
+
+
 def pca_transform(img, k):
-    import numpy as np
     from sklearn.decomposition import PCA, IncrementalPCA
 
     img_array = np.array(img)
@@ -74,11 +130,11 @@ def pca_transform(img, k):
 
 
 def get_data_loaders(
-    img_dim=(784, 784),
+    img_dim=(300, 300),
     fingerprint_database="1",
     single_class=False,
     validation_split=0.25,
-    shuffle_dataset=True,
+    shuffle_dataset=False,
     batch_size=2,
     pca_transform=False,
     train_class=None,
@@ -91,12 +147,19 @@ def get_data_loaders(
         pca_transform=pca_transform,
     )
     dataset_size = len(dataset)
+    classes_indices = {}
+    for i in range(dataset_size):
+        class_id = dataset[i][1].item()
+        classes_indices.setdefault(class_id, []).append(i)
+
+    classes = sorted(list(classes_indices.keys()))
     indices = list(range(dataset_size))
-    classes = list(set([dataset[i][1].item() for i in range(dataset_size)]))
     print(f"Dataset size: {dataset_size}")
     print(f"Dataset classes: {classes}")
     img_size = dataset[0][0].size()
     print(f"Image size: {img_size}")
+    for class_id in classes_indices.keys():
+        print(f"Class {class_id} indices: {classes_indices[class_id]}")
 
     # in case of single class whole dataset contains only one class
     if single_class:
@@ -104,7 +167,6 @@ def get_data_loaders(
             random.shuffle(indices)
         split = int(validation_split * dataset_size)
         train_indices, test_indices = indices[split:], indices[:split]
-
     else:
         if not train_class:
             train_class = random.choice(classes)
@@ -119,13 +181,9 @@ def get_data_loaders(
                     f"Test class {test_class} not in dataset classes, available classes: {classes}"
                 )
 
-        for i in range(dataset_size):
-            print(f"indice:{i}: {dataset[i][1].item()}")
-
         print(f"train class: {train_class}, test class: {test_class}")
-        train_indices = [
-            i for i in range(dataset_size) if dataset[i][1].item() == train_class
-        ]
+        train_indices = classes_indices[train_class]
+        print(f"Train indices: {train_indices}")
         if train_class == test_class:
             indices = train_indices
             split = int(validation_split * len(indices))
@@ -137,6 +195,7 @@ def get_data_loaders(
             test_indices = [
                 i for i in range(dataset_size) if dataset[i][1].item() == test_class
             ]
+            test_indices = classes_indices[test_class]
             test_split = int(validation_split * len(test_indices))
             train_split = int(validation_split * len(train_indices))
             if shuffle_dataset:
@@ -164,7 +223,7 @@ def get_data_loaders(
 
 if __name__ == "__main__":
     train_loader, test_loader, _, _ = get_data_loaders(
-        single_class=False, test_class=101, shuffle_dataset=True
+        single_class=False, test_class=101, shuffle_dataset=False
     )
 
     for loader in [train_loader, test_loader]:
@@ -176,6 +235,7 @@ if __name__ == "__main__":
                 label = label.item()
                 fingerprints.setdefault(label, []).append(img)
 
+        print(f"Classes: {fingerprints.keys()}")
         width = max([len(fingerprints[key]) for key in fingerprints.keys()])
         height = fingerprints.__len__()
         # reduce size to make it more readable
